f3479e5a4fdd75d48665a920986729fc2e4f58ff
Rafael Bastos <rafael.bastos.96930@gmail.com>
2020-08-26 17:04:27 -0300

    Two fuctions (makeCacheMatrix) and (cacheSolve) to generate the data

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions created to prompt the user for matrix, store it,
+## generate the inverse form of the one prompted, and then print
+## and store the result
 
-## Write a short comment describing this function
+## This function stores the original matrix prompted and it's inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matriz = matrix()) {
+        inverse_form <- NULL
+        
+        set_original_matrix <- function(new_matriz){
+            matriz <<- new_matriz
+            inverse_form <<- NULL
+        }
+        
+        get_original_matrix <- function(){matriz}
+        
+        set_inverse <- function(inver){
+            inverse_form <<- inver
+        }
+        
+        get_inverse <- function(){inverse_form}
+        
+        list(set_original_matrix = set_original_matrix,
+             get_original_matrix = get_original_matrix,
+             set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
+## This function proceeds to search within the cache for the inverse data
+## and print it. If not found just calculate, print and store
+## (Also, JoJo references...)
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrix_object, ...){
+        if (!is.null(matrix_object$get_inverse())){
+            print("I, Giorno Giovanna, will get your cache...")
+            print("...")
+            return(matrix_object$get_inverse())
+        }
+        else{
+            print("I, Giorno Giovanna, couldn't find a cache...")
+            print("Proceeding to calculate it")
+            data <- matrix_object$get_original_matrix()
+            result <- solve(data)
+            matrix_object$set_inverse(result)
+            print("...")
+            print("Here is your inverse matrix: ")
+            result
+            }
 }
